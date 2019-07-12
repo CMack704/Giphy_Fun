f4fdac6f570cda90afc9a6792641d001cf832a11
Cmack704 <craigmack704@gmail.com>
2019-07-11 21:08:19 -0400

    added more to my js and css

--- assets/giphy.js
+++ assets/giphy.js
@@ -2,7 +2,13 @@
 var topics = ["dogs", "gaming", "basketball", "football", "rocket league", "soccer", "game of thrones","NASA", "Universe"]
 
 //Api key PkzLNR7ZPXGvM0fBXS0c8tDme5snUNQZ
-
+var queryURL = "http://api.giphy.com/v1/gifs/search?q=dog&api_key=PkzLNR7ZPXGvM0fBXS0c8tDme5snUNQZ&limit=5"
+$.ajax({
+    url: queryURL,
+    method: "GET"
+}).then(function(response){
+    console.log(response.data[.0.images.original.url]);
+})
 
 
 //function to get response from giphy and disply in html
@@ -29,11 +35,11 @@ var topics = ["dogs", "gaming", "basketball", "football", "rocket league", "socc
 
        
 
-            var newImage1 = $("<img src='" + stillGiphy1 + "'>");
-            var newImage2 = $("<img src='" + stillGiphy2 + "'>");
-            var newImage3 = $("<img src='" + stillGiphy3 + "'>");
-            var newImage4 = $("<img src='" + stillGiphy4 + "'>");
-            var newImage5 = $("<img src='" + stillGiphy5 + "'>");
+            var newImage1 = $("<img src='" + stillGiphy1 + "' data-still='" + stillGiphy1 + "' data-animated='" + Giphy1 + "' data-state='still'>");
+            var newImage2 = $("<img src='" + stillGiphy2 + "' data-still='" + stillGiphy2 + "' data-animated='" + Giphy2 + "' data-state='still'>");
+            var newImage3 = $("<img src='" + stillGiphy3 + "' data-still='" + stillGiphy3 + "' data-animated='" + Giphy3 + "' data-state='still'>");
+            var newImage4 = $("<img src='" + stillGiphy4 + "' data-still='" + stillGiphy4 + "' data-animated='" + Giphy4 + "' data-state='still'>");
+            var newImage5 = $("<img src='" + stillGiphy5 + "' data-still='" + stillGiphy5 + "' data-animated='" + Giphy5 + "' data-state='still'>");
 
             $("#topics").append(newImage1);
             $("#topics").append(newImage2);
@@ -51,8 +57,38 @@ var topics = ["dogs", "gaming", "basketball", "football", "rocket league", "socc
             var button = $("<button>");
             button.addClass("topics-button");
             button.attr("topic-data", topics[i]);
+            button.attr("data-state");
+            button.attr("data-animate");
+            button.attr("data-still");
             button.text(topics[i]);
             $("#topicsButton").append(button);
+        };
+    };
+
+
+    $("#add-topic").on("click", function(event){
+        event.preventDefault();
+        var topic = $("#topic-input").val().trim();
+        topics.push(topic);
+        makeButtons();
+    });
+
+
+    $(".topics.button").on("click", function(){
+        var state = $(this).attr("data-state");
+        var animatedGiph = $(this).attr("data-animated");
+        var stillGiph = $(this).attr("data-still");
+
+        if (state === "still") {
+            $(this).attr("src", animatedGiph);
+            $(this).attr("data-state", "animate");
+        } else {
+            $(this).attr("src", stillGiph);
+            $(this).attr("data-state", "still");
         }
-    }
+    });
+
+    $(document).on("click", ".topics-button", giphyResponse);
+
+
     makeButtons();
