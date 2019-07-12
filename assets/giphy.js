d84f8f8dca712a8543317c3cdde6df4efc6b0361
Cmack704 <craigmack704@gmail.com>
2019-07-11 22:35:55 -0400

    buttons now bring up giphy images and you can click to animate them

--- assets/giphy.js
+++ assets/giphy.js
@@ -1,17 +1,11 @@
-//array of my favorite topics
+
 var topics = ["dogs", "gaming", "basketball", "football", "rocket league", "soccer", "game of thrones","NASA", "Universe"]
 
 //Api key PkzLNR7ZPXGvM0fBXS0c8tDme5snUNQZ
-var queryURL = "http://api.giphy.com/v1/gifs/search?q=dog&api_key=PkzLNR7ZPXGvM0fBXS0c8tDme5snUNQZ&limit=5"
-$.ajax({
-    url: queryURL,
-    method: "GET"
-}).then(function(response){
-    console.log(response.data[.0.images.original.url]);
-})
 
 
-//function to get response from giphy and disply in html
+
+
     function giphyResponse() {
 
         var topic = $(this).attr("topic-data")
@@ -21,31 +15,82 @@ $.ajax({
             url: queryURL,
             method: "GET"
         }).then(function(response){
-            var stillGiphy1 = response.data[.0.images.original_still.url];
-            var stillGiphy2 = response.data[.1.images.original_still.url];
-            var stillGiphy3 = response.data[.2.images.original_still.url];
-            var stillGiphy4 = response.data[.3.images.original_still.url];
-            var stillGiphy5 = response.data[.4.images.original_still.url];
+            var stillGiphy = [response.data[0].images.original_still.url,
+             response.data[1].images.original_still.url,
+             response.data[2].images.original_still.url,
+             response.data[3].images.original_still.url,
+             response.data[4].images.original_still.url]
 
-            var Giphy1 = response.data[.0.images.original.url];
-            var Giphy2 = response.data[.1.images.original.url];
-            var Giphy3 = response.data[.2.images.original.url];
-            var Giphy4 = response.data[.3.images.original.url];
-            var Giphy5 = response.data[.4.images.original.url];
+            var giphy = [response.data[0].images.original.url,
+             response.data[1].images.original.url,
+             response.data[2].images.original.url,
+             response.data[3].images.original.url,
+             response.data[4].images.original.url]
 
        
 
-            var newImage1 = $("<img src='" + stillGiphy1 + "' data-still='" + stillGiphy1 + "' data-animated='" + Giphy1 + "' data-state='still'>");
-            var newImage2 = $("<img src='" + stillGiphy2 + "' data-still='" + stillGiphy2 + "' data-animated='" + Giphy2 + "' data-state='still'>");
-            var newImage3 = $("<img src='" + stillGiphy3 + "' data-still='" + stillGiphy3 + "' data-animated='" + Giphy3 + "' data-state='still'>");
-            var newImage4 = $("<img src='" + stillGiphy4 + "' data-still='" + stillGiphy4 + "' data-animated='" + Giphy4 + "' data-state='still'>");
-            var newImage5 = $("<img src='" + stillGiphy5 + "' data-still='" + stillGiphy5 + "' data-animated='" + Giphy5 + "' data-state='still'>");
+            var newImage1 = $("<img class='gif' src='" + stillGiphy[0] + "' data-still='" + stillGiphy[0] + "' data-animated='" + giphy[0] + "' data-state='still'>");
+            var newImage2 = $("<img class='gif'  src='" + stillGiphy[1] + "' data-still='" + stillGiphy[1] + "' data-animated='" + giphy[1] + "' data-state='still'>");
+            var newImage3 = $("<img class='gif'  src='" + stillGiphy[2] + "' data-still='" + stillGiphy[2] + "' data-animated='" + giphy[2] + "' data-state='still'>");
+            var newImage4 = $("<img class='gif'  src='" + stillGiphy[3] + "' data-still='" + stillGiphy[3] + "' data-animated='" + giphy[3] + "' data-state='still'>");
+            var newImage5 = $("<img class='gif'  src='" + stillGiphy[4] + "' data-still='" + stillGiphy[4] + "' data-animated='" + giphy[4] + "' data-state='still'>");
 
             $("#topics").append(newImage1);
             $("#topics").append(newImage2);
             $("#topics").append(newImage3);
             $("#topics").append(newImage4);
             $("#topics").append(newImage5);
+
+            $(newImage1).on("click", function(){                
+                var state = $(this).attr("data-state")
+                if (state === "still") {
+                    $(this).attr("src", giphy[0]);
+                    $(this).attr("data-state", "animate");
+                } else {
+                    $(this).attr("src", stillGiphy[0]);
+                    $(this).attr("data-state", "still");
+                }                
+            });
+            $(newImage2).on("click", function(){                
+                var state = $(this).attr("data-state")
+                if (state === "still") {
+                    $(this).attr("src", giphy[1]);
+                    $(this).attr("data-state", "animate");
+                } else {
+                    $(this).attr("src", stillGiphy[1]);
+                    $(this).attr("data-state", "still");
+                }                
+            });
+            $(newImage3).on("click", function(){                
+                var state = $(this).attr("data-state")
+                if (state === "still") {
+                    $(this).attr("src", giphy[2]);
+                    $(this).attr("data-state", "animate");
+                } else {
+                    $(this).attr("src", stillGiphy[2]);
+                    $(this).attr("data-state", "still");
+                }                
+            });
+            $(newImage4).on("click", function(){                
+                var state = $(this).attr("data-state")
+                if (state === "still") {
+                    $(this).attr("src", giphy[3]);
+                    $(this).attr("data-state", "animate");
+                } else {
+                    $(this).attr("src", stillGiphy[3]);
+                    $(this).attr("data-state", "still");
+                }                
+            });
+            $(newImage5).on("click", function(){                
+                var state = $(this).attr("data-state")
+                if (state === "still") {
+                    $(this).attr("src", giphy[4]);
+                    $(this).attr("data-state", "animate");
+                } else {
+                    $(this).attr("src", stillGiphy[4]);
+                    $(this).attr("data-state", "still");
+                }                
+            });
         });
 
     
@@ -74,19 +119,7 @@ $.ajax({
     });
 
 
-    $(".topics.button").on("click", function(){
-        var state = $(this).attr("data-state");
-        var animatedGiph = $(this).attr("data-animated");
-        var stillGiph = $(this).attr("data-still");
-
-        if (state === "still") {
-            $(this).attr("src", animatedGiph);
-            $(this).attr("data-state", "animate");
-        } else {
-            $(this).attr("src", stillGiph);
-            $(this).attr("data-state", "still");
-        }
-    });
+    
 
     $(document).on("click", ".topics-button", giphyResponse);
 
